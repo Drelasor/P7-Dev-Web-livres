f7138f83746b4907b2a8ef8eaeb44125d9439807
Raphaël Grand <otumbarg@gmail.com>
2024-05-24 17:47:32 +0200

    correction erreur model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-  email: { String, required: true, unique: true },
-  password: { String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: {type: String, required: true },
 });
 
 userSchema.plugin(uniqueValidator);
